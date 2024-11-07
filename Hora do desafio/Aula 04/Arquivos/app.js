86a4cfdbcb86a0c18f120e2d321da3e654d49160
julioTiTE <jcaetite@hotmail.com>
2024-11-07 16:27:47 -0300

    Add files via upload

---
+++ Hora do desafio/Aula 04/Arquivos/app.js
@@ -0,0 +1,37 @@
+﻿const Livro = require('./livro');
+
+// Criando dois livros com diferentes atributos
+const livro1 = new Livro("O Senhor dos Anéis", "J.R.R. Tolkien", 1954);
+const livro2 = new Livro("1984", "George Orwell", 1949);
+
+// Exibindo as informações dos livros
+console.log("Informações do Livro 1:");
+livro1.exibirInformacoes();
+console.log();
+
+console.log("Informações do Livro 2:");
+livro2.exibirInformacoes();
+console.log();
+
+// Tentando emprestar o livro1
+console.log("Tentando emprestar o Livro 1...");
+livro1.emprestar();
+console.log();
+
+// Exibindo novamente as informações dos livros para verificar a disponibilidade
+console.log("Informações do Livro 1 após emprestar:");
+livro1.exibirInformacoes();
+console.log();
+
+console.log("Informações do Livro 2 após emprestar Livro 1:");
+livro2.exibirInformacoes();
+console.log();
+
+// Tentando devolver o livro1
+console.log("Tentando devolver o Livro 1...");
+livro1.devolver();
+console.log();
+
+
+console.log("Informações do Livro 1 após devolver:");
+livro1.exibirInformacoes();
---
+++ Hora do desafio/Aula 04/Arquivos/livro.js
@@ -0,0 +1,31 @@
+﻿class Livro {
+    constructor(titulo, autor, anoPublicacao) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+        this.disponivel = true; // Inicialmente, todos os livros estão disponíveis
+    }
+
+    emprestar() {
+        if (this.disponivel) {
+            this.disponivel = false;
+            console.log(`O livro "${this.titulo}" foi emprestado.`);
+        } else {
+            console.log(`O livro "${this.titulo}" não está disponível.`);
+        }
+    }
+
+    devolver() {
+        this.disponivel = true;
+        console.log(`O livro "${this.titulo}" foi devolvido.`);
+    }
+
+    exibirInformacoes() {
+        console.log(`Título: ${this.titulo}`);
+        console.log(`Autor: ${this.autor}`);
+        console.log(`Ano de Publicação: ${this.anoPublicacao}`);
+        console.log(`Disponível: ${this.disponivel ? "Sim" : "Não"}`);
+    }
+}
+
+module.exports = Livro;
